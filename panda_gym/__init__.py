0b99541e99dab442b63c3e1c64be59005c643a2d
Tanguy-M <tanguy.merle00@gmail.com>
2022-12-08 19:45:18 +0100

    Update __init__.py

--- panda_gym/__init__.py
+++ panda_gym/__init__.py
@@ -52,3 +52,10 @@ for reward_type in ["sparse", "dense"]:
             kwargs=kwargs,
             max_episode_steps=50,
         )
+
+	register(
+            id="DoosanTest{}{}-v3".format(control_suffix, reward_suffix),
+            entry_point="panda_gym.envs:DoosanTest",
+            kwargs=kwargs,
+            max_episode_steps=100,
+        )
